bcc44d32625fef8d5eb6aa7aea87cfc5460f380d
3RmaNo <130006956+3rmano@users.noreply.github.com>
2023-04-25 21:55:43 +0100

    new

--- philo_bonus/death_bonus.c
+++ philo_bonus/death_bonus.c
@@ -6,7 +6,7 @@
 /*   By: namorgha <namorgha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 12:28:46 by namorgha          #+#    #+#             */
-/*   Updated: 2023/04/25 19:19:11 by namorgha         ###   ########.fr       */
+/*   Updated: 2023/04/25 20:55:08 by namorgha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,11 @@ void	*check_time_of_death(void *j)
 		if ((philo->last_meal + philo->time_to_die) < get_time())
 		{
 			if (!(*philo->pointer))
+			{
+				sem_wait(philo->print);
 				printf("%lld %d died\n", curr_time(philo), philo->id);
-			philo->died = 1;
+				philo->died = 1;
+			}
 			exit(1);
 		}
 		usleep(1000);
